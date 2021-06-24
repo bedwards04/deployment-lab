a22696a7fce8e203f3d2812bfd14957f4e6201bf
Brendan Edwards <bdawg@Brendans-MacBook-Air.local>
2021-06-24 16:27:51 -0600

    added get request to see if the image would pull up on website

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use("/index.css", express.static(path.join(__dirname, '/public/index.css')));
-app.use("/Brendan.jpg", express.static(path.join(__dirname, '/public/Brendan.jpg')))
+
 
 app.get('/', function(req, res) {
     
     res.sendFile( path.join(__dirname, '../index.html') );
 });
+app.get('/picture', function(req, res) {
+    res.sendFile('Brendan.jpg')
+})
 
 const port = process.env.PORT || 4050;
 app.listen(port, () => {console.log(`Server running on ${port}`)});
