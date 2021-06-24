394c4d7bfa422db689aad21879ecd7d5b974b982
Brendan Edwards <bdawg@Brendans-MacBook-Air.local>
2021-06-24 16:00:25 -0600

    altered static function to see if it will serve image and css file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('deployment'))
+app.use(express.static(__dirname + './public'));
 
 app.get('/', function(req, res) {
     
